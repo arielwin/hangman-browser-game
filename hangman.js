2c46604878b21c06e8e4d8bfb00e4e9638be63c2
arielwin <subiboy7@gmail.com>
2024-06-21 13:46:29 -0600

    fleshed out code and documents further

--- hangman.js
+++ hangman.js
@@ -15,7 +15,9 @@ const word_list = ["engine", "transmission", "brake", "accelerator", "clutch", "
 
 //picking the random word from the list using the random word fucntion
 const game_word = randomWord(word_list)
+var chosenLetter = ''
 
+const keys = document.querySelectorAll('.key')
 //----------------Functions-------------------------
 
 //gets a random word from the word list
@@ -23,3 +25,18 @@ function randomWord(list) {
     const idx = Math.floor(Math.random() * word_list.length)
     return list[idx]
 }
+
+//function to reset the game after win/loss
+function reset () {
+
+}
+
+//--------Listeners-------------------------------
+
+//keyboard clicks
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        chosenLetter = (key.textContent);
+        console.log(`You clicked ${chosenLetter}`)
+    })
+})
